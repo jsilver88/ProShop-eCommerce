2d6947dddc318822a66d9f3ef48dd73b58aac3bf
Jared Silver <jrsilver88@gmail.com>
2020-10-06 19:46:57 -0700

    set up models in db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import color from 'colors';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -25,5 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
